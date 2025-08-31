6f804b4fb18941e7b53b8f635ec6db782034a95e
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2025-08-31 09:09:39 +0700

    chore: initial import of shadcn-docs-nuxt

---
+++ components/content/Stack.vue
@@ -0,0 +1,12 @@
+<template>
+  <UiCard class=" overflow-hidden border-0 bg-white rounded-xl hover:shadow-lg [&:not(:first-child)]:mt-5">
+    <div v-for="(slot, i) in $slots.default?.() ?? []" :key="i" class="bg-purple-100">
+      <component :is="slot" :in-stack="true" class="rounded-b" />
+    </div>
+  </UiCard>
+
+</template>
+
+<script setup lang="ts">
+defineSlots();
+</script>
---
+++ components/ui/card/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    :class="
+      cn(
+        'rounded-lg border-0 bg-card text-card-foreground shadow-sm',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import { cn } from '@/lib/utils';
+
+const props = defineProps<{
+  class?: HTMLAttributes['class'];
+}>();
+</script>
